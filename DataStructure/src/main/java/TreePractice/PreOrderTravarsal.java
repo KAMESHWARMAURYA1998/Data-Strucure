cd9a007391f94a128ace6f51db86c329e187e12c
Kameshwar Maurya <80618989+KAMESHWARMAURYA1998@users.noreply.github.com>
2022-05-26 00:44:37 +0530

    implementation of singly linked list

---
+++ DataStructure/src/main/java/TreePractice/PreOrderTravarsal.java
@@ -0,0 +1,37 @@
+package TreePractice;
+
+/**
+	* @author Kameshwar Maurya
+	*/
+public class PreOrderTravarsal {
+
+				TreeNode root;
+
+				class TreeNode {
+
+								int data;
+								TreeNode next;
+								TreeNode prev;
+
+								TreeNode(int data) {
+												this.data = data;
+								}
+
+				}
+				public void createNode() {
+								TreeNode t1 = new TreeNode(9);
+								TreeNode t2 = new TreeNode(4);
+								TreeNode t3 = new TreeNode(3);
+								TreeNode t4 = new TreeNode(2);
+
+								root = t1;
+								t1.next = t2;
+								t1.prev = t3;
+								t2.next = t4;
+				}
+
+				public static void main(String[] args){
+
+				}
+
+}
---
+++ DataStructure/src/main/java/data/structure/topic/list/SinglyLinkedList.java
@@ -0,0 +1,19 @@
+package data.structure.topic.list;
+
+/**
+	* @author Kameshwar Maurya
+	*/
+public class SinglyLinkedList {
+
+				private ListNode head;
+
+				public static class ListNode{
+						private int data;
+						private ListNode next;
+
+						public ListNode(int data){
+										this.data = data;
+										this.next = null;
+						}
+				}
+}
